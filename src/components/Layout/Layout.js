417112440c7d1e1a3f46b7a4dccfa2d15a5395cb
ritoshyadav <ritosh90@gmail.com>
2019-02-02 10:34:02 +0530

    initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+const layout = (props) => (
+    <Aux>
+        <Toolbar />
+        <main className="Content">
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
